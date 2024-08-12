a1e05915f9d52d9dcaab678436340607e008988c
fraolb <fraolbereket@gmail.com>
2024-08-12 11:31:52 +0300

    claim function that create a merkle hash and transfer the tokens

--- src/BagelToken.sol
+++ src/BagelToken.sol
@@ -1,4 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract BagelToken {}
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BagelToken is ERC20, Ownable {
+    constructor() ERC20("Bagel", "BAGEL") Ownable(msg.sender) {}
+}
--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,14 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 /**
  * @title Merkle Airdrop
  * @author Fraol Bereket
  * @notice This contract takes list of addresses and allow the addresses to claim tokens
  */
-contract MerkleAirdrop is ERC20, Ownable {
-    constructor() ERC20("Bagel", "BAGEL") Ownable(msg.sender) {}
+contract MerkleAirdrop {
+    using SafeERC20 for IERC20;
+
+    error MerkleAirdrop__InvalidProof();
+
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+
+    event Claim(address, uint256);
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function cliam(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        // we have to hash and concat, to avoid collistions. It avoids Second preimage atatck.
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+    }
 }
