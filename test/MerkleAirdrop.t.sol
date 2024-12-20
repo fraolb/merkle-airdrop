53e6b665e0df42dd115f9b61d93f241b7d7f4342
fraolb <fraolbereket@gmail.com>
2024-12-21 00:08:37 +0300

    test sign message

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -11,13 +11,16 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
     MerkleAirdrop public merkleAirdrop;
     BagelToken public bagelToken;
 
-    bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    bytes32 public ROOT =
+        0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     uint256 public AMOUNT_TO_CLAIM = 25 * 1e18;
     uint256 public AMOUNT_TO_SEND = 4 * AMOUNT_TO_CLAIM;
 
     // Initialize the proofs directly in the array
-    bytes32 public proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
-    bytes32 public proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32 public proofOne =
+        0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 public proofTwo =
+        0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] public PROOF = [proofOne, proofTwo];
 
     address public gasPayer;
@@ -37,6 +40,17 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
 
         (user, userPrivKey) = makeAddrAndKey("user");
         gasPayer = makeAddr("gasPayer");
+    }
+
+    function signMessage(
+        uint256 privKey,
+        address account
+    ) public view returns (uint8 v, bytes32 r, bytes32 s) {
+        bytes32 hashedMessage = airdrop.getMessageHash(
+            account,
+            amountToCollect
+        );
+        (v, r, s) = vm.sign(privKey, hashedMessage);
     }
 
     function testUserCanClaim() public {
